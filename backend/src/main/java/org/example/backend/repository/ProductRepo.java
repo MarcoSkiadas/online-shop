e2d2fcddae393fe25502639eca9bfe116bc7660a
MarcoSkiadas <Skiadas2@hotmail.com>
2024-07-18 11:01:34 +0200

    Fixing Repos

--- backend/src/main/java/org/example/backend/repository/ProductRepo.java
+++ backend/src/main/java/org/example/backend/repository/ProductRepo.java
@@ -2,6 +2,8 @@ package org.example.backend.repository;
 
 import org.example.backend.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepo extends MongoRepository<Product, String> {
 }
--- backend/src/main/java/org/example/backend/repository/ShoppingListRepo.java
+++ backend/src/main/java/org/example/backend/repository/ShoppingListRepo.java
@@ -2,6 +2,8 @@ package org.example.backend.repository;
 
 import org.example.backend.model.ShoppingList;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShoppingListRepo extends MongoRepository<ShoppingList, String> {
 }

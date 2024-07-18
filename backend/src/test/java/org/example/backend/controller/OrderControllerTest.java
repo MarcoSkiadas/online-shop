19cb1ad5698971d0c605469ce418beec1747647f
MarcoSkiadas <Skiadas2@hotmail.com>
2024-07-18 17:52:48 +0200

    Adding GetOrdersById Backend+Tests

--- backend/src/test/java/org/example/backend/controller/OrderControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/OrderControllerTest.java
@@ -1,7 +1,6 @@
 package org.example.backend.controller;
 
 import org.example.backend.model.Order;
-import org.example.backend.model.Product;
 import org.example.backend.repository.OrderRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;

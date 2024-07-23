1adc9f527c80ab78c3ec1b01ce65ec0d3149d884
MarcoSkiadas <Skiadas2@hotmail.com>
2024-07-23 16:00:17 +0200

    Build Frontend for removeProduct

--- frontend/src/pages/ShoppingCartPage.tsx
+++ frontend/src/pages/ShoppingCartPage.tsx
@@ -2,10 +2,15 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import {ShoppingCart} from "../components/ShopSchema.ts";
 
+
 export default function ShoppingCartPage() {
     const [shoppingCart, setShoppingCartList] = useState<ShoppingCart[]>();
 
     useEffect(() => {
+        getShoppingCart()
+    }, []);
+
+    function getShoppingCart() {
         axios.get(`/api/shoppingCart`)
             .then(response => {
                 setShoppingCartList(response.data);
@@ -13,7 +18,15 @@ export default function ShoppingCartPage() {
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
-    }, []);
+    }
+
+    function removeProductFromShoppingCart(productId:string)  {
+        console.log(`Remove product with id: ${productId}`)
+        axios.put("/api/shoppingCart/removeProduct/233e77ed-1d3a-4ee5-ada0-5704129f34f6", { productIds: [productId] })
+            .then(response => console.log(response.data))
+            .then(getShoppingCart)
+            .catch(error => console.log(error))
+    }
 
     return (
         <>
@@ -23,9 +36,15 @@ export default function ShoppingCartPage() {
             {shoppingCart?.map(shoppingCart => (
                 <div key={shoppingCart.id}>
                     <p>Id: {shoppingCart.id}</p>
-                    <p>ProductIds: {shoppingCart.productIds.map(productIds => (
-                        <li>{productIds}</li>
-                    ))}</p>
+                    <p>ProductIds:</p>
+                    <ul>
+                        {shoppingCart.productIds.map(productId => (
+                            <li key={productId}>
+                                {productId}
+                                <button onClick={() => removeProductFromShoppingCart(productId)}>Remove Button</button>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             ))}
         </>

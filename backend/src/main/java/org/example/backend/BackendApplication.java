77f312d00bae35956b860ece84d54eccf542c2e0
MarcoSkiadas <Skiadas2@hotmail.com>
2024-08-06 17:14:52 +0200

    Adding Frontend for Reviews

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -9,9 +9,6 @@ public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-        Cloudinary cloudinary = new Cloudinary(System.getenv("CLOUDINARY_URL"));
-        cloudinary.config.secure = true;
-        System.out.println(cloudinary.config.cloudName);
     }
 
 }

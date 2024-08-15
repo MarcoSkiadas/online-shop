1a6e4d3ff0593db1785c1c61031ba021811e8c3a
MarcoSkiadas <Skiadas2@hotmail.com>
2024-08-15 13:47:28 +0200

    Adjust Styling issue fix

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -6,11 +6,9 @@ export default function AdminPage(props: Readonly<AdminPageProps>) {
 
 
     return (
-        <>
-            <div className="action-page">
-                <button onClick={props.handleProductButton} className={"action-button"}>Product</button>
-                <button onClick={props.handleOrderButton} className={"action-button"}>Order</button>
-            </div>
-        </>
+        <div className="action-page">
+            <button onClick={props.handleProductButton} className={"action-button"}>Product</button>
+            <button onClick={props.handleOrderButton} className={"action-button"}>Order</button>
+        </div>
     )
 }
